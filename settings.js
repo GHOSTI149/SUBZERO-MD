df1b773d9d619689caaceca232bdd8b526de10b9
GHOSTI149 <youngtrillaa56@gmail.com>
2025-08-03 13:14:43 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO-MD;;;edtAXKpL#MV4uX5ifbwKI49sVXTBgJW5QUzgQ1WgJLcRZrk0FrQ4", // Put Session Id Here Inside "*"
+  SESSION_ID: "SUBZERO-MD;;;LRFwWTzI#ziwDaRH3l2hxSqtFe7-1wj7Jq7783DE4TbctP4p9jTk", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
